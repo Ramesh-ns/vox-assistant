31090b4cda720d9e45d88b99311c8e269f41f0db
Ramesh <ramesh.yenugula@gmail.com>
2023-05-20 13:19:26 -0700

    Add react front end

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import Controller from "./components/Controller"
+
+function App() {
+
+  return (
+    <>
+      <div className='text-2xl bg-blue-500'>
+       <Controller />
+      </div>
+    </>
+  )
+}
+
+export default App
